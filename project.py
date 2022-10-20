af38d6f4273188da3eca4c5e1f88313a5274c73d
Tomadio <116260584+Tomadio@users.noreply.github.com>
2022-10-20 21:10:29 +1300

    Project.py

---
+++ project.py
@@ -0,0 +1,35 @@
+name = input ("What would you like to be called? ")
+print ("hello", name)
+age = input ("What is your age? ")
+
+q1 = input ("you see a cave and a waterfall, type 1 to enter the cave type 2 to investigate the waterfall ")
+
+
+if q1 == "1":
+    q2 = input ("You see a little creature type 1 to befriend it type 2 to kill it ")
+
+if q2 == "1":
+    print ("You have found yourself a friend for your short life")
+    
+if q2 == "1":
+    cname = input ("What would you like to name the creature? ")
+    
+if q2 == "1":
+    print ("You've named the creature... ", cname )
+
+if q2 == "2":
+    print ("You feel a bit guilty but at least you have some food")
+    
+if q3 == "2":
+    print ("You tried to go forward but stumbled upon a pack of hyenas.... GAME OVER")
+    
+if q3 == "1":
+    print ("you dont want to go back into the cave so you go to the waterfall")
+    
+
+
+
+if q2 == "1":
+    print ("you go to investigate the waterfall but your creature pushes you off.... GAME OVER")
+
+
